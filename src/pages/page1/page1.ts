f9ee2d734dd63bb0aeaa59c839b2c21c5d5839ff
Cooper Mayne <coopermayne@gmail.com>
2017-04-20 18:12:18 -0700

    better toast functionality

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -20,6 +20,7 @@ export class Page1 {
   question;
   imgIndex = 0;
   img;
+  toast;
   buttonsDisabled:boolean = false;
 
   ionViewDidLoad() {
@@ -40,31 +41,42 @@ export class Page1 {
   }
 
   clickAnswer(correct) {
-    this.buttonsDisabled = !this.buttonsDisabled
-    if(correct){
-      var message = "correct";
-      var toastClass = "correct";
-      var duration = 2000;
+    if ( !this.buttonsDisabled ){
+      //first try
+      console.log('first try')
+      this.buttonsDisabled = !this.buttonsDisabled
+
+      if(correct){
+        var message = "correct";
+        var toastClass = "correct";
+        var duration = 2000;
+      } else {
+        var message = "incorrect - note correct answer";
+        var toastClass = "incorrect";
+        var duration = 0;  //0 means untill dismiss
+      }
+
+      this.toast = this.toastCtrl.create({
+        message: message,
+        duration: duration,
+        position: "top",
+        showCloseButton: true,
+        cssClass: toastClass
+      });
+
+      this.toast.onDidDismiss(() => {
+        this.setUpQuestion()
+        this.buttonsDisabled = false
+      });
+
+      this.toast.present();
+
     } else {
-      var message = "incorrect - note correct answer";
-      var toastClass = "incorrect";
-      var duration = 10000;
+      //second try
+      console.log('second try')
+      this.toast.dismiss();
     }
 
-    let toast = this.toastCtrl.create({
-      message: message,
-      duration: duration,
-      position: "top",
-      showCloseButton: true,
-      cssClass: toastClass
-    });
-
-    toast.onDidDismiss(() => {
-      this.setUpQuestion()
-      this.buttonsDisabled = !this.buttonsDisabled
-    });
-
-    toast.present();
   }
 
   setUpQuestion() {
